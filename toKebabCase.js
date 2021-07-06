28c4ca37c40e43b33ab1ccbc5c3c516df88420a3
jinook929 <jinook929@gmail.com>
2021-07-05 23:11:58 -0400

    update toKebabCase.js

--- toKebabCase.js
+++ toKebabCase.js
@@ -1,4 +1,5 @@
 function toKebabCase(str) {
+    console.log(`${str.replace(/\s/ig, "-").toLowerCase()}.py`)
     return `${str.replace(/\s/ig, "-").toLowerCase()}.js`
 }
-console.log(toKebabCase( "Merge Two Sorted Lists" ))
+console.log(toKebabCase( "Palindrome Linked List" ))
