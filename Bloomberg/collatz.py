2bd50e6a1bc32c2aa01461579907d4a08360b4e5
jinook929 <jinook929@gmail.com>
2021-08-07 21:23:07 -0400

    added question comments to collatz problem

--- Bloomberg/collatz.py
+++ Bloomberg/collatz.py
@@ -1,3 +1,11 @@
+# target = 1
+# even / 2
+# odd * 3 + 1
+
+# 2 => 2/2=1 [1]
+# 3 => 3*3+1=10 => 10/2=5 => 5*3+1=16 => 16/2=8 => 8/2=4 => 4/2=2 => 2/2=1 [7]
+# 4 => 4/2=2 => 2/2=1 [2]
+
 class Collatz:
   def __init__(self):
     self.cache = {}
@@ -11,16 +19,16 @@ class Collatz:
   #   return counter
 
   ### memoization version
-  def numSteps(self, n):
-    if n in self.cache:
-      return self.cache[n]
-    key = n
-    counter = 0
-    while n != 1:
-      n = n / 2 if n % 2 == 0 else n * 3 + 1
-      counter += 1
-    self.cache[key] = counter
-    return counter
+  # def numSteps(self, n):
+  #   if n in self.cache:
+  #     return self.cache[n]
+  #   key = n
+  #   counter = 0
+  #   while n != 1:
+  #     n = n / 2 if n % 2 == 0 else n * 3 + 1
+  #     counter += 1
+  #   self.cache[key] = counter
+  #   return counter
     
   ### recursion version
   # def numSteps(self, n, counter = 0):
@@ -53,11 +61,11 @@ print(c.numSteps(3))
 print(c.numSteps(4))
 print(c.numSteps(8))
 print(c.numSteps(63_728_127))
-for _ in range(44444):
+for _ in range(4444):
   c.numSteps(670_617_279)
 print(c.numSteps(670_617_279))
-print(c.numSteps(9_780_657_631))
-for _ in range(4444444):
-  c.numSteps(75_128_138_247)
-print(c.numSteps(75_128_138_247))
-print(c.cache)
+# print(c.numSteps(9_780_657_631))
+# for _ in range(444444):
+#   c.numSteps(75_128_138_247)
+# print(c.numSteps(75_128_138_247))
+# print(c.cache)
